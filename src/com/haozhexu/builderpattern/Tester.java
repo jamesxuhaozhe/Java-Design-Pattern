a4d58317d636862f308ad2854d9be8211f662b9e
Haozhe Xu <jamesxuhaozhe@gmail.com>
2015-10-19 00:36:15 -0700

    builder pattern demo

---
+++ src/com/haozhexu/builderpattern/Tester.java
@@ -0,0 +1,13 @@
+package com.haozhexu.builderpattern;
+
+public class Tester {
+
+	public static void main(final String[] args) {
+		Car mycar = new Car.CarBuilder("Convertable", "Red")
+						.setCountry("China")
+						.setMake("Toyota")
+						.setYear("2015")
+						.build();
+		mycar.run();
+	}
+}
