c39448b7e42eb3ee0181d4c1833637642fe992be
James Xu <jamesxuhaozhe@gmail.com>
2020-12-01 21:32:36 +0800

    Add some demo for dynamic proxy

--- src/com/haozhexu/demo/main.java
+++ src/com/haozhexu/demo/main.java
@@ -1,27 +1,53 @@
 package com.haozhexu.demo;
 
-import java.util.*;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.sql.Timestamp;
 
 public class main {
 
-    private static class Data {
-        private int num;
 
-        private Data(int num) {
-            this.num = num;
+
+    public static void main(String[] args) {
+
+        UserService service = new UserServiceImpl();
+        MyInvocationHandler handler = new MyInvocationHandler(service);
+        UserService proxy = (UserService) handler.getProxy();
+        proxy.add();
+        System.currentTimeMillis();
+    }
+
+    interface UserService {
+        void add();
+    }
+
+
+    static class UserServiceImpl implements UserService {
+        @Override
+        public void add() {
+            System.out.println("UserServiceImpl add method gets called");
+        }
+    }
+
+    static class MyInvocationHandler implements InvocationHandler {
+
+        private Object target;
+
+        MyInvocationHandler(Object target) {
+            this.target = target;
         }
 
 
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            Object result = method.invoke(target, args);
+            return result;
+        }
+
+        public Object getProxy() {
+            return Proxy.newProxyInstance(Thread.currentThread().getContextClassLoader(), target.getClass().getInterfaces(), this);
+        }
     }
 
-    public static void main(String[] args) {
-        List<Data> arrayList = new ArrayList<>();
-        arrayList.add(new Data(1));
-        arrayList.add(new Data(2));
-        arrayList.add(new Data(3));
-        arrayList.add(new Data(4));
-
-        List<Data> umoList = Collections.unmodifiableList(arrayList);
-        umoList.get(0).num++;
-    }
 }
