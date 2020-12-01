caff769c619beb2fe9f6f8d31499954987e72eb4
James Xu <jamesxuhaozhe@gmail.com>
2020-12-01 22:26:25 +0800

    Add more demo for time

--- src/com/haozhexu/demo/main.java
+++ src/com/haozhexu/demo/main.java
@@ -4,6 +4,11 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.time.*;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class main {
 
@@ -16,6 +21,37 @@ public class main {
         UserService proxy = (UserService) handler.getProxy();
         proxy.add();
         System.currentTimeMillis();
+
+
+        Date date = new Date();
+
+        Calendar ca = Calendar.getInstance();
+
+        ca.setTime(date);
+        ca.add(Calendar.DATE, 1);
+        date = ca.getTime();
+        System.out.println(date);
+
+        LocalDateTime ldt = LocalDateTime.now();
+        LocalDateTime oneDayLater = ldt.plusDays(1L);
+        System.out.println(oneDayLater);
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        sdf.setTimeZone(TimeZone.getTimeZone("America/Los_Angeles"));
+        System.out.println(sdf.format(Calendar.getInstance().getTime()));
+
+
+        LocalDateTime localDateTime = LocalDateTime.now(ZoneId.of("America/Los_Angeles"));
+        System.out.println(localDateTime);
+
+
+        Period period = Period.between(LocalDate.of(2020, 12, 1), LocalDate.of(2020, 12, 29));
+        System.out.println(period.getYears() + " " + period.getMonths() +" " + period.getDays());
+
+
+
+
+
     }
 
     interface UserService {
